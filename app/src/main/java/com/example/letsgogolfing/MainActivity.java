a38ece7b77fcbf2a2a5e1962825a0325cc835540
Vedant Talati <vedanttalati@gmail.com>
2023-11-06 13:04:12 -0700

    gaslit
    jordygaslit

--- app/src/main/java/com/example/letsgogolfing/MainActivity.java
+++ app/src/main/java/com/example/letsgogolfing/MainActivity.java
@@ -2,22 +2,31 @@ package com.example.letsgogolfing;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.letsgogolfing.model.ItemAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 
 import com.example.letsgogolfing.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-//import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-//    private FirebaseFirestore db;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +35,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.main_page);
 
-        db = FirebaseFirestore.getInstance();
-        gridView = findViewById(R.id.itemGrid);
-        itemAdapter = new ItemAdapter(this, itemsList);
-        gridView.setAdapter(itemAdapter);
-
-        // click listener for add item
+        /*/ --- click listener for add item ---
         Button addItemButton = findViewById(R.id.addItemButton);
         addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,21 +45,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Fetch items from Firebase Firestore
-        db.collection("items").get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    Item item = document.toObject(Item.class);
-                    itemsList.add(item);
-                }
-                itemAdapter.notifyDataSetChanged(); // Notify the adapter about the data change
-            } else {
-                // Handle the error
-            }
-        });
+         */
 
-
-//        db = FirebaseFirestore.getInstance(); // get our database from FireStore on app launch
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
