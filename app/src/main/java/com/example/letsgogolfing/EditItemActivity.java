c68b46a6f5d601a03b203edbf4d09acac0e872e4
Jordan Isaac <102484814+jordanisaac1@users.noreply.github.com>
2023-11-08 22:11:45 -0700

    ensure persistence doesnt mess with testing
    sometimes data cache messes with functionality if you input the wrong data type into the database

--- app/src/main/java/com/example/letsgogolfing/EditItemActivity.java
+++ app/src/main/java/com/example/letsgogolfing/EditItemActivity.java
@@ -3,10 +3,12 @@ package com.example.letsgogolfing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -14,31 +16,101 @@ import java.util.Map;
 
 public class EditItemActivity extends AppCompatActivity {
 
+    private static final String TAG = "EditItemActivity"; // logging purposes
+
+    // Field for storing the item ID if we are editing
+    private String itemId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_item_page);
 
+        // Check if we're in edit mode
+        itemId = getIntent().getStringExtra("ITEM_ID");
+        if (itemId != null && !itemId.isEmpty()) {
+            loadItemData(itemId);
+        }
+
         Button confirmBtn = findViewById(R.id.confirmBtn);
-        confirmBtn.setOnClickListener(v -> saveItem());
+        confirmBtn.setOnClickListener(v -> {
+            if (itemId != null && !itemId.isEmpty()) {
+                updateItem(itemId); // Update existing item
+            } else {
+                saveNewItem(); // Save new item
+            }
+        });
     }
 
-    private void saveItem() {
-        String name = ((EditText) findViewById(R.id.nameField)).getText().toString();
-        String description = ((EditText) findViewById(R.id.descriptionField)).getText().toString();
-        // Continue for all fields...
+    private void loadItemData(String itemId) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("items").document(itemId).get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    Item item = documentSnapshot.toObject(Item.class);
+                    if (item != null) {
+                        ((EditText) findViewById(R.id.nameField)).setText(item.getName());
+                        ((EditText) findViewById(R.id.descriptionField)).setText(item.getDescription());
+                    }
+                })
+                .addOnFailureListener(e -> Toast.makeText(EditItemActivity.this, "Error loading item", Toast.LENGTH_SHORT).show());
+    }
 
-        Map<String, Object> item = new HashMap<>();
-        item.put("name", name);
-        item.put("description", description);
-        // Continue putting other fields...
+    private void updateItem(String itemId) {
+        Map<String, Object> item = getItemDataFromForm();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("items").document(itemId).set(item)
+                .addOnSuccessListener(aVoid -> {
+                    Log.d(TAG, "Item updated successfully");
+                    Toast.makeText(EditItemActivity.this, "Item updated", Toast.LENGTH_SHORT).show();
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    Log.e(TAG, "Error updating item", e);
+                    Toast.makeText(EditItemActivity.this, "Error updating item", Toast.LENGTH_SHORT).show();
+                });
+    }
 
+    private void saveNewItem() {
+        Map<String, Object> item = getItemDataFromForm();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("items").add(item)
                 .addOnSuccessListener(documentReference -> {
+                    Log.d(TAG, "Item added successfully");
                     Toast.makeText(EditItemActivity.this, "Item added", Toast.LENGTH_SHORT).show();
-                    finish(); // Close the activity, returning to MainActivity
+                    finish();
                 })
-                .addOnFailureListener(e -> Toast.makeText(EditItemActivity.this, "Error adding item", Toast.LENGTH_SHORT).show());
+                .addOnFailureListener(e -> {
+                    Log.e(TAG, "Error adding item", e);
+                    Toast.makeText(EditItemActivity.this, "Error adding item", Toast.LENGTH_SHORT).show();
+                });
     }
+
+    private Map<String, Object> getItemDataFromForm() {
+        Map<String, Object> item = new HashMap<>();
+        item.put("name", ((EditText) findViewById(R.id.nameField)).getText().toString());
+        item.put("description", ((EditText) findViewById(R.id.descriptionField)).getText().toString());
+        item.put("dateOfPurchase", ((EditText) findViewById(R.id.dateField)).getText().toString()); // Consider converting this to a Date object or Timestamp
+        item.put("make", ((EditText) findViewById(R.id.makeField)).getText().toString());
+        item.put("model", ((EditText) findViewById(R.id.modelField)).getText().toString());
+        item.put("serialNumber", ((EditText) findViewById(R.id.serialField)).getText().toString());
+        item.put("comment", ((EditText) findViewById(R.id.commentField)).getText().toString());
+        item.put("tags", ((EditText) findViewById(R.id.tagsField)).getText().toString()); // This should be a List<String>, not a single string
+
+        // Parse the estimated value as a double
+        try {
+            double estimatedValue = Double.parseDouble(((EditText) findViewById(R.id.valueField)).getText().toString());
+            item.put("estimatedValue", estimatedValue);
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Invalid number format for estimated value", Toast.LENGTH_LONG).show();
+            // You might want to return or handle this case differently
+        }
+
+        // Convert the date string to a Date object or Firebase Timestamp before putting it into the map
+
+
+        // Convert the tags string to a List<String> before putting it into the map
+
+        return item;
+    }
+
 }
--- app/src/main/java/com/example/letsgogolfing/MainActivity.java
+++ app/src/main/java/com/example/letsgogolfing/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
@@ -30,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         itemGrid.setAdapter(itemAdapter);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // clear persistence
+        db.clearPersistence(); // IMPORTANT, TESTING USE ONLY
+
         db.collection("items").addSnapshotListener((queryDocumentSnapshots, e) -> {
             if (e != null) {
                 Toast.makeText(MainActivity.this, "Error loading items", Toast.LENGTH_SHORT).show();
@@ -43,6 +49,17 @@ public class MainActivity extends AppCompatActivity {
 
             itemAdapter.updateItems(items);
         });
+
+        // Inside onCreate after setting the adapter
+        itemGrid.setOnItemClickListener((parent, view, position, id) -> {
+            Item item = itemAdapter.getItem(position);
+            Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+            intent.putExtra("EDIT_ITEM", true); // Flag to indicate editing
+            intent.putExtra("ITEM_ID", item.getId()); // Pass the unique Firestore document ID
+            // ... pass other item properties
+            startActivity(intent);
+        });
+
 
         ImageView addItemButton = findViewById(R.id.addItemButton);
         addItemButton.setOnClickListener(v -> {
