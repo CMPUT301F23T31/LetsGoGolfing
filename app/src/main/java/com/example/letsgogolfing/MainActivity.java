e9847333b20b2372ed4eee354b676cea9f3209b4
Jordan Isaac <102484814+jordanisaac1@users.noreply.github.com>
2023-11-08 21:07:47 -0700

    Added functional xml's

--- app/src/main/java/com/example/letsgogolfing/MainActivity.java
+++ app/src/main/java/com/example/letsgogolfing/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.letsgogolfing;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView itemGrid;
+    private ItemAdapter itemAdapter; // You need to create this Adapter class.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        itemGrid = findViewById(R.id.itemGrid);
+        itemAdapter = new ItemAdapter(this, new ArrayList<>());
+        itemGrid.setAdapter(itemAdapter);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("items").addSnapshotListener((queryDocumentSnapshots, e) -> {
+            if (e != null) {
+                Toast.makeText(MainActivity.this, "Error loading items", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            List<Item> items = new ArrayList<>();
+            for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                items.add(doc.toObject(Item.class)); // Assuming you have an Item class
+            }
+
+            itemAdapter.updateItems(items);
+        });
+
+        ImageView addItemButton = findViewById(R.id.addItemButton);
+        addItemButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+            startActivity(intent);
+        });
     }
 }
