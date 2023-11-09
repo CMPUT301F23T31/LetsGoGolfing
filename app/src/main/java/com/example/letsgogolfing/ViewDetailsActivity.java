838fc2179d43325001d77cbdef585ab1681af792
Jordan Isaac <102484814+jordanisaac1@users.noreply.github.com>
2023-11-09 09:16:26 -0700

    View item, and edit item, and save edits all functional and working

--- app/src/main/java/com/example/letsgogolfing/ViewDetailsActivity.java
+++ app/src/main/java/com/example/letsgogolfing/ViewDetailsActivity.java
@@ -2,13 +2,23 @@ package com.example.letsgogolfing;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class ViewDetailsActivity extends AppCompatActivity {
 
@@ -55,6 +65,93 @@ public class ViewDetailsActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
+
+        Button saveButton = findViewById(R.id.saveBtn);
+        Button editButton = findViewById(R.id.editInfoBtn);
+        editButton.setOnClickListener(v -> {
+            // sets all of the fields to enabled
+            ((EditText) findViewById(R.id.nameField)).setEnabled(true);
+            ((EditText) findViewById(R.id.descriptionField)).setEnabled(true);
+            ((EditText) findViewById(R.id.modelField)).setEnabled(true);
+            ((EditText) findViewById(R.id.makeField)).setEnabled(true);
+            ((EditText) findViewById(R.id.serialField)).setEnabled(true);
+            ((EditText) findViewById(R.id.commentField)).setEnabled(true);
+            ((EditText) findViewById(R.id.dateField)).setEnabled(true);
+            ((EditText) findViewById(R.id.valueField)).setEnabled(true);
+
+            saveButton.setVisibility(v.VISIBLE);
+
+        });
+
+        saveButton.setOnClickListener(v -> {
+            // Extract the updated information from EditText fields
+            String updatedName = ((EditText) findViewById(R.id.nameField)).getText().toString();
+            String updatedDescription = ((EditText) findViewById(R.id.descriptionField)).getText().toString();
+            String updatedMake = ((EditText) findViewById(R.id.makeField)).getText().toString();
+            String updatedModel = ((EditText) findViewById(R.id.modelField)).getText().toString();
+            String updatedSerialNumber = ((EditText) findViewById(R.id.serialField)).getText().toString();
+            String updatedComment = ((EditText) findViewById(R.id.commentField)).getText().toString();
+            String updatedDate = ((EditText) findViewById(R.id.dateField)).getText().toString();
+            String updatedValueString = ((EditText) findViewById(R.id.valueField)).getText().toString();
+
+            // Convert the date string back to a Date object
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            Date updatedDateOfPurchase = null;
+            try {
+                updatedDateOfPurchase = sdf.parse(updatedDate);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                Toast.makeText(ViewDetailsActivity.this, "Invalid date format", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Convert the value string to a double
+            double updatedEstimatedValue;
+            try {
+                updatedEstimatedValue = Double.parseDouble(updatedValueString);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                Toast.makeText(ViewDetailsActivity.this, "Invalid number format for estimated value", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Create a Map for the updated values
+            Map<String, Object> updatedValues = new HashMap<>();
+            updatedValues.put("name", updatedName);
+            updatedValues.put("description", updatedDescription);
+            updatedValues.put("make", updatedMake);
+            updatedValues.put("model", updatedModel);
+            updatedValues.put("serialNumber", updatedSerialNumber);
+            updatedValues.put("comment", updatedComment);
+            updatedValues.put("dateOfPurchase", updatedDateOfPurchase != null ? new Timestamp(updatedDateOfPurchase) : null);
+            updatedValues.put("estimatedValue", updatedEstimatedValue);
+
+            // Get the document ID from the item
+            String documentId = item.getId(); // Assuming 'item' is an instance variable representing the current item
+
+            // Update Firestore document
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            db.collection("items").document(documentId)
+                    .update(updatedValues)
+                    .addOnSuccessListener(aVoid -> {
+                        Toast.makeText(ViewDetailsActivity.this, "Changes saved", Toast.LENGTH_SHORT).show();
+                        // Disable EditTexts and hide the save button again
+                        ((EditText) findViewById(R.id.nameField)).setEnabled(false);
+                        ((EditText) findViewById(R.id.descriptionField)).setEnabled(false);
+                        ((EditText) findViewById(R.id.modelField)).setEnabled(false);
+                        ((EditText) findViewById(R.id.makeField)).setEnabled(false);
+                        ((EditText) findViewById(R.id.serialField)).setEnabled(false);
+                        ((EditText) findViewById(R.id.commentField)).setEnabled(false);
+                        ((EditText) findViewById(R.id.dateField)).setEnabled(false);
+                        ((EditText) findViewById(R.id.valueField)).setEnabled(false);
+                        saveButton.setVisibility(View.INVISIBLE);
+                    })
+                    .addOnFailureListener(e -> {
+                        e.printStackTrace();
+                        Toast.makeText(ViewDetailsActivity.this, "Error updating item", Toast.LENGTH_SHORT).show();
+                    });
+        });
+
 
 
 
