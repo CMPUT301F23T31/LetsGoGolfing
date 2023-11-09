5226fa5175c55c1df5f4615fa350476c8f43cda7
levi <levi.thomas5384@gmail.com>
2023-11-09 15:56:17 -0700

    Added Date and Decimal Formatting in Formatters class

---
+++ app/src/main/java/com/example/letsgogolfing/utils/Formatters.java
@@ -0,0 +1,14 @@
+package com.example.letsgogolfing.utils;
+
+import android.text.TextUtils;
+
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+
+public class Formatters {
+    public static final DecimalFormat decimalFormat = new DecimalFormat("#,###.##");
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+}
