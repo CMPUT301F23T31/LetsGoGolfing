486b84f8df25d66178d69728e97064d3750ec1da
Jordan Isaac <102484814+jordanisaac1@users.noreply.github.com>
2023-11-10 15:29:37 -0700

    added javadocs

--- app/JavaDoc/package-search-index.js
+++ app/JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.letsgogolfing"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.letsgogolfing"},{"l":"com.example.letsgogolfing.utils"}];updateSearchResults();
