832d420a042d43385d051d875d975614bb898e83
levi <levi.thomas5384@gmail.com>
2023-11-24 18:59:27 -0700

    Split ViewDetails and EditItem functionality into two activities.

--- app/src/main/java/com/example/letsgogolfing/EditItemActivity.java
+++ app/src/main/java/com/example/letsgogolfing/EditItemActivity.java
@@ -1,16 +1,211 @@
 package com.example.letsgogolfing;
 
-import android.widget.EditText;
+import static com.example.letsgogolfing.utils.Formatters.dateFormat;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EditItemActivity extends AppCompatActivity {
-    EditText name;
-    EditText description;
-    EditText value;
-    EditText make;
-    EditText model;
-    EditText serial;
-    EditText comment;
-    EditText date;
+    private EditText name;
+    private EditText description;
+    private EditText value;
+    private EditText make;
+    private EditText model;
+    private EditText serial;
+    private EditText comment;
+    private EditText date;
+    private LinearLayout tagsContainerView;
+    private Button saveButton;
+    private Button cancelButton;
+    private Button addPhotoButton;
+    private Button addTagsButton;
+    private ImageButton backButton;
+    private List<String> tagList = new ArrayList<>(); // This should be populated from Firestore
+    private List<String> selectedTags = new ArrayList<>();
+    private Item item;
+    private static final String TAG = "EditItemActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_item);
+
+        // Retrieve the item from the intent
+        item = (Item) getIntent().getSerializableExtra("ITEM");
+
+
+        InitializeEditTextAndButtons(item);
+
+        backButton.setOnClickListener(v -> {
+            // takes back to home page main_activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        cancelButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ViewDetailsActivity.class);
+            startActivity(intent);
+        });
+
+        addTagsButton.setOnClickListener(v -> {
+            showTagSelectionDialog();
+        });
+
+        addPhotoButton.setOnClickListener(v -> {
+            // Implement Add Photo
+        });
+
+        saveButton.setOnClickListener(v -> {
+            updateItem();
+            // Use New FirestoreRepository class
+        });
+    }
+
+
+    private void InitializeEditTextAndButtons(Item item) {
+        // Initialize EditTexts
+        name = findViewById(R.id.name_edit_text);
+        description = findViewById(R.id.description_edit_text);
+        make = findViewById(R.id.make_edit_text);
+        model = findViewById(R.id.model_edit_text);
+        serial = findViewById(R.id.serial_edit_text);
+        comment = findViewById(R.id.comment_edit_text);
+        date = findViewById(R.id.date_edit_text);
+        value = findViewById(R.id.value_edit_text);
+        tagsContainerView = findViewById(R.id.tags_linear_layout);
+        // Initialize Buttons
+        addTagsButton = findViewById(R.id.add_tags_button_view);
+        saveButton = findViewById(R.id.save_button);
+        cancelButton = findViewById(R.id.cancel_edit_button);
+        addPhotoButton = findViewById(R.id.add_photo_button);
+        backButton = findViewById(R.id.back_button);
+
+        // Set the EditTexts with the original values
+        name.setText(item.getName());
+        description.setText(item.getDescription());
+        make.setText(item.getMake());
+        model.setText(item.getModel());
+        serial.setText(item.getSerialNumber());
+        comment.setText(item.getComment());
+        date.setText(dateFormat.format(item.getDateOfPurchase()));
+        value.setText(Double.toString(item.getEstimatedValue()));
+        loadTags();
+    }
+
+    private void displayTags() {
+        tagsContainerView.removeAllViews(); // Clear all views/tags before adding new ones
+
+        for (String tag : selectedTags) {
+            TextView tagView = new TextView(this);
+            tagView.setText(tag);
+            tagView.setBackgroundResource(R.drawable.tag_background); // Make sure this drawable exists
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+            params.setMargins(0, 0, 16, 0); // Adding some right margin between tags
+
+            tagView.setLayoutParams(params);
+            tagsContainerView.addView(tagView); // Add the TextView to your container
+
+        }
+    }
+
+
+    private void loadTags() {
+        // Assuming you have a method to fetch tags from Firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("tags").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                tagList.clear();
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    tagList.add(document.getString("name"));
+                }
+                selectedTags = new ArrayList<>(item.getTags()); // Use the tags from the item
+                displayTags();
+            } else {
+                Log.w(TAG, "Error getting documents: ", task.getException());
+            }
+        });
+    }
+
+    private void showTagSelectionDialog() {
+        // Convert List to array for AlertDialog
+        String[] tagsArray = tagList.toArray(new String[0]);
+        boolean[] checkedTags = new boolean[tagList.size()];
+        for (int i = 0; i < tagList.size(); i++) {
+            checkedTags[i] = selectedTags.contains(tagList.get(i));
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMultiChoiceItems(tagsArray, checkedTags, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                // Add or remove the tag from the selected tags list based on whether the checkbox is checked
+                String selectedTag = tagList.get(which);
+                if (isChecked) {
+                    selectedTags.add(selectedTag);
+                } else {
+                    selectedTags.remove(selectedTag);
+                }
+            }
+        });
+
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            displayTags(); // Update the display with the selected tags
+        });
+        builder.setNegativeButton("Cancel", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void updateItem() {
+        try {
+            // Convert the date string back to a Date object
+            item.setDateOfPurchase(dateFormat.parse(date.getText().toString()));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Toast.makeText(EditItemActivity.this, "Invalid date format", Toast.LENGTH_SHORT).show();
+        }
+
+        // Convert the value string to a double
+        try {
+            item.setEstimatedValue(Double.parseDouble(value.getText().toString()));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            Toast.makeText(EditItemActivity.this, "No Value Entered. Defaulted to 0.", Toast.LENGTH_SHORT).show();
+            item.setEstimatedValue(0);
+        }
+        item.setName(name.getText().toString());
+        item.setDescription(description.getText().toString());
+        item.setModel(model.getText().toString());
+        item.setMake(make.getText().toString());
+        item.setSerialNumber(serial.getText().toString());
+        item.setComment(comment.getText().toString());
+        item.setTags(selectedTags);
+    }
 }
--- app/src/main/java/com/example/letsgogolfing/ViewDetailsActivity.java
+++ app/src/main/java/com/example/letsgogolfing/ViewDetailsActivity.java
@@ -2,65 +2,40 @@ package com.example.letsgogolfing;
 
 import static com.example.letsgogolfing.utils.Formatters.dateFormat;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ViewDetailsActivity extends AppCompatActivity {
+    private TextView name;
+    private TextView description;
+    private TextView value;
+    private TextView make;
+    private TextView model;
+    private TextView serial;
+    private TextView comment;
+    private TextView date;
+    private LinearLayout tagsContainerView;
 
-
-    private String originalName;
-    private String originalDescription;
-    private String originalMake;
-    private String originalModel;
-    private String originalSerial;
-    private String originalComment;
-    private Date originalDate;
-
-    private double originalValue;
-    private String originalTags;
-    EditText name;
-    EditText description;
-    EditText value;
-    EditText make;
-    EditText model;
-    EditText serial;
-    EditText comment;
-    EditText date;
-
-    Button editButton;
-    Button viewPhotoButton;
-    Button saveButton;
-    Button cancelButton;
-    Button addPhotoButton;
-    ImageButton backButton;
+    private Button editButton;
+    private Button viewPhotoButton;
+    private ImageButton backButton;
 
     private List<String> tagList = new ArrayList<>(); // This should be populated from Firestore
     private List<String> selectedTags = new ArrayList<>();
     private Item item;
-    private List<String> originalTagsList = new ArrayList<>();
     private static final String TAG = "ViewDetailsActivity";
 
 
@@ -69,26 +44,69 @@ public class ViewDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_details);
 
+        InitializeUI();
+
+        backButton.setOnClickListener(v -> {
+            // takes back to home page main_activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        editButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, EditItemActivity.class);
+            intent.putExtra("ITEM", item);
+            startActivity(intent);
+        });
+
+        viewPhotoButton.setOnClickListener(v -> {
+            // Implement View Photos
+        });
+    }
+
+    private void InitializeUI() {
         // Retrieve the item from the intent
         item = (Item) getIntent().getSerializableExtra("ITEM");
 
+        // Instantiate TextViews
+        name = findViewById(R.id.name_view_text);
+        description = findViewById(R.id.description_view_text);
+        make = findViewById(R.id.make_view_text);
+        model = findViewById(R.id.model_view_text);
+        serial = findViewById(R.id.serial_view_text);
+        comment = findViewById(R.id.comment_view_text);
+        date = findViewById(R.id.date_view_text);
+        value = findViewById(R.id.value_view_text);
 
-        InitializeEditTextAndButtons(item);
+        // Instantiate Buttons
+        editButton = findViewById(R.id.edit_item_button);
+        backButton = findViewById(R.id.back_button);
+        viewPhotoButton = findViewById(R.id.view_photo_button);
 
-        // MAYBE DON"T NEED
-        // list of tags
-        List<String> tags = item.getTags();
-        //String tagsString = TextUtils.join(", ", tags);
+        // Instantiate Tag Container
+        tagsContainerView = findViewById(R.id.tags_linear_layout);
 
+        // Set content TextViews
+        name.setText(item.getName());
+        description.setText(item.getDescription());
+        make.setText(item.getMake());
+        model.setText(item.getModel());
+        serial.setText(item.getSerialNumber());
+        comment.setText(item.getComment());
+        date.setText(dateFormat.format(item.getDateOfPurchase()));
+        value.setText(Double.toString(item.getEstimatedValue()));
 
-        LinearLayout tagsContainerView = findViewById(R.id.tags_linear_layout);
+        // Set content of Tag Container
+        loadTags();
+    }
+
+    private void displayTags() {
+        tagsContainerView = findViewById(R.id.tags_linear_layout);
         tagsContainerView.removeAllViews(); // Clear all views/tags before adding new ones
 
-        for (String tag : tags) {
+        for (String tag : selectedTags) {
             TextView tagView = new TextView(this);
             tagView.setText(tag);
             tagView.setBackgroundResource(R.drawable.tag_background); // Make sure this drawable exists
-            // Set other TextView properties like padding, textAppearance, etc.
 
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -98,214 +116,9 @@ public class ViewDetailsActivity extends AppCompatActivity {
 
             tagView.setLayoutParams(params);
             tagsContainerView.addView(tagView); // Add the TextView to your container
-        }
-
-        backButton.setOnClickListener(v -> {
-            // takes back to home page main_activity
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        });
-
-        editButton.setOnClickListener(v -> {
-            TransitionToEdit(v);
-        });
-
-        cancelButton.setOnClickListener(v -> {
-            selectedTags = new ArrayList<>(originalTagsList);
-            displayTags();
-            SetFieldsToOriginalValues(v);
-            TransitionToViewItem(v);
-        });
-
-        Button addTagsButton = findViewById(R.id.add_tags_button_view);
-        addTagsButton.setOnClickListener(v -> {
-            showTagSelectionDialog();
-        });
-
-        loadTags();
-
-        saveButton.setOnClickListener(v -> {
-            // Extract the updated information from EditText fields
-            String updatedName = name.getText().toString();
-            String updatedDescription = description.getText().toString();
-            String updatedMake = make.getText().toString();
-            String updatedModel = model.getText().toString();
-            String updatedSerialNumber = serial.getText().toString();
-            String updatedComment = comment.getText().toString();
-            String updatedDate = date.getText().toString();
-            String updatedValueString = value.getText().toString();
-
-            Date updatedDateOfPurchase = null;
-            try {
-                // Convert the date string back to a Date object
-                updatedDateOfPurchase = dateFormat.parse(updatedDate);
-            } catch (ParseException e) {
-                e.printStackTrace();
-                Toast.makeText(ViewDetailsActivity.this, "Invalid date format", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            // Convert the value string to a double
-            double updatedEstimatedValue;
-            try {
-                updatedEstimatedValue = Double.parseDouble(updatedValueString);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-                Toast.makeText(ViewDetailsActivity.this, "No Value Entered. Defaulted to 0.", Toast.LENGTH_SHORT).show();
-                updatedEstimatedValue = 0;
-
-                return;
-            }
-
-            // Create a Map for the updated values
-            Map<String, Object> updatedValues = new HashMap<>();
-            updatedValues.put("name", updatedName);
-            updatedValues.put("description", updatedDescription);
-            updatedValues.put("make", updatedMake);
-            updatedValues.put("model", updatedModel);
-            updatedValues.put("serialNumber", updatedSerialNumber);
-            updatedValues.put("comment", updatedComment);
-            updatedValues.put("dateOfPurchase", updatedDateOfPurchase != null ? new Timestamp(updatedDateOfPurchase) : null);
-            updatedValues.put("estimatedValue", updatedEstimatedValue);
-            updatedValues.put("tags", selectedTags);
-
-            // Get the document ID from the item
-            String documentId = item.getId(); // Assuming 'item' is an instance variable representing the current item
-
-            // Update Firestore document
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            db.collection("items").document(documentId)
-                    .update(updatedValues)
-                    .addOnSuccessListener(aVoid -> {
-                        Toast.makeText(ViewDetailsActivity.this, "Changes saved", Toast.LENGTH_SHORT).show();
-                        // Update originalTagsList to reflect the newly saved tags
-                        originalTagsList = new ArrayList<>(selectedTags);
-                        TransitionToViewItem(v);
-
-                    })
-                    .addOnFailureListener(e -> {
-                        e.printStackTrace();
-                        Toast.makeText(ViewDetailsActivity.this, "Error updating item", Toast.LENGTH_SHORT).show();
-                    });
-        });
-    }
-
-
-    private void TransitionToEdit(View v) {
-        saveButton.setVisibility(v.VISIBLE);
-        cancelButton.setVisibility(v.VISIBLE);
-        addPhotoButton.setVisibility(v.VISIBLE);
-        viewPhotoButton.setVisibility(v.INVISIBLE);
-        editButton.setVisibility(v.INVISIBLE);
-        name.setEnabled(true);
-        description.setEnabled(true);
-        model.setEnabled(true);
-        make.setEnabled(true);
-        serial.setEnabled(true);
-        comment.setEnabled(true);
-        date.setEnabled(true);
-        value.setEnabled(true);
-        // make the add/edit tags button visible
-        Button addTagsButton = findViewById(R.id.add_tags_button_view);
-        addTagsButton.setVisibility(View.VISIBLE);
-    }
-    private void TransitionToViewItem(View v) {
-        saveButton.setVisibility(v.INVISIBLE);
-        cancelButton.setVisibility(v.INVISIBLE);
-        addPhotoButton.setVisibility(v.INVISIBLE);
-        viewPhotoButton.setVisibility(v.VISIBLE);
-        editButton.setVisibility(v.VISIBLE);
-        name.setEnabled(false);
-        description.setEnabled(false);
-        model.setEnabled(false);
-        make.setEnabled(false);
-        serial.setEnabled(false);
-        comment.setEnabled(false);
-        date.setEnabled(false);
-        value.setEnabled(false);
-        // Hide the add/edit tags button
-        Button addTagsButton = findViewById(R.id.add_tags_button_view);
-        addTagsButton.setVisibility(View.INVISIBLE);
-    }
-
-    private void SetFieldsToOriginalValues(View v) {
-        name.setText(originalName);
-        description.setText(originalDescription);
-        make.setText(originalMake);
-        model.setText(originalModel);
-        serial.setText(originalSerial);
-        comment.setText(originalComment);
-        date.setText(dateFormat.format(originalDate));
-        value.setText(Double.toString(originalValue));
-    }
-
-    private void InitializeEditTextAndButtons(Item item) {
-
-        // Initialize EditTexts
-        name = findViewById(R.id.name_edit_text);
-        description = findViewById(R.id.description_edit_text);
-        make = findViewById(R.id.make_edit_text);
-        model = findViewById(R.id.model_edit_text);
-        serial = findViewById(R.id.serial_edit_text);
-        comment = findViewById(R.id.comment_edit_text);
-        date = findViewById(R.id.date_edit_text);
-        value = findViewById(R.id.value_edit_text);
-        // Initialize Buttons
-        saveButton = findViewById(R.id.save_button);
-        editButton = findViewById(R.id.editInfoBtn);
-        cancelButton = findViewById(R.id.cancel_edit_button);
-        addPhotoButton = findViewById(R.id.add_photo_button);
-        backButton = findViewById(R.id.back_button);
-        viewPhotoButton = findViewById(R.id.viewPhotoBtn);
-
-        // Set original values for when cancel is pressed
-        originalName = item.getName();
-        originalDescription = item.getDescription();
-        originalMake = item.getMake();
-        originalModel = item.getModel();
-        originalSerial = item.getSerialNumber();
-        originalComment = item.getComment();
-        originalDate = item.getDateOfPurchase();
-        originalValue = item.getEstimatedValue();
-        originalTagsList = new ArrayList<>(item.getTags());
-
-
-        // Set the EditTexts with the original values
-        name.setText(originalName);
-        description.setText(originalDescription);
-        make.setText(originalMake);
-        model.setText(originalModel);
-        serial.setText(originalSerial);
-        comment.setText(originalComment);
-        date.setText(dateFormat.format(originalDate));
-        value.setText(Double.toString(originalValue));
-
-        // Set fields to not be editable at first
-        name.setEnabled(false);
-        description.setEnabled(false);
-        model.setEnabled(false);
-        make.setEnabled(false);
-        serial.setEnabled(false);
-        comment.setEnabled(false);
-        date.setEnabled(false);
-        value.setEnabled(false);
-    }
-
-    private void displayTags() {
-        LinearLayout tagsContainerView = findViewById(R.id.tags_linear_layout);
-        tagsContainerView.removeAllViews(); // Clear all views/tags before adding new ones
-
-        for (String tag : selectedTags) {
-
-            TextView tagView = new TextView(this);
-            tagView.setText(tag);
-            tagView.setBackgroundResource(R.drawable.tag_background); // Make sure this drawable exists
-            // Add LayoutParams, margins, etc., here
-            tagsContainerView.addView(tagView); // Add the TextView to your container
 
         }
     }
-
 
     private void loadTags() {
         // Assuming you have a method to fetch tags from Firestore
@@ -322,36 +135,5 @@ public class ViewDetailsActivity extends AppCompatActivity {
                 Log.w(TAG, "Error getting documents: ", task.getException());
             }
         });
-    }
-
-    private void showTagSelectionDialog() {
-        // Convert List to array for AlertDialog
-        String[] tagsArray = tagList.toArray(new String[0]);
-        boolean[] checkedTags = new boolean[tagList.size()];
-        for (int i = 0; i < tagList.size(); i++) {
-            checkedTags[i] = selectedTags.contains(tagList.get(i));
-        }
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMultiChoiceItems(tagsArray, checkedTags, new DialogInterface.OnMultiChoiceClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                // Add or remove the tag from the selected tags list based on whether the checkbox is checked
-                String selectedTag = tagList.get(which);
-                if (isChecked) {
-                    selectedTags.add(selectedTag);
-                } else {
-                    selectedTags.remove(selectedTag);
-                }
-            }
-        });
-
-        builder.setPositiveButton("OK", (dialog, which) -> {
-            displayTags(); // Update the display with the selected tags
-        });
-        builder.setNegativeButton("Cancel", null);
-        AlertDialog dialog = builder.create();
-        dialog.show();
-
     }
 }
